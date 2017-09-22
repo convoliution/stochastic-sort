15e02086e88385387992efc9c385f089203c7f21
Michael Liu <siphe24@gmail.com>
2017-09-22 16:09:01 -0700

    create isSorted()

--- utils.py
+++ utils.py
@@ -1,5 +1,29 @@
 import numpy as np
 
+def isSorted(array: np.ndarray) -> bool:
+    '''Evaluates whether or not a one-dimensional np.ndarray is sorted.
+
+    Args:
+        array: Array to be evaluated.
+
+    Returns:
+        Score earned by `array`.
+
+    Raises:
+        TypeError: If `array` is of a type other than np.ndarray.
+        ValueError: If `array` is of a dimension other than 1.
+
+    '''
+    if type(array) != np.ndarray:
+        raise TypeError("argument must be of type np.ndarray; argument was type {} instead".format(type(array)))
+    if array.ndim != 1:
+        raise ValueError("argument must be of dimension 1")
+
+    for i in range(array.size - 1):
+        if array[i] > array[i+1]:
+            return False
+    return True
+
 def score(array: np.ndarray) -> int:
     '''Quantitatively evaluate the sortedness of a one-dimensional np.ndarray.
 
