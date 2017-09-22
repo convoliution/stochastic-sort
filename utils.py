b63ca30716294e7fa0710dc4b229c3d39251ae0f
Michael Liu <siphe24@gmail.com>
2017-09-22 16:06:11 -0700

    rename evaluate() to score()

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def evaluate(array: np.ndarray) -> int:
-    '''Evaluate the sortedness of a np.ndarray.
+def score(array: np.ndarray) -> int:
+    '''Quantitatively evaluate the sortedness of a np.ndarray.
 
     More positive scores are given to arrays in ascending order;
     More negative scores are given to arrays in descending order.
@@ -21,9 +21,10 @@ def evaluate(array: np.ndarray) -> int:
         raise TypeError("argument must be of type np.ndarray; argument was type {} instead".format(type(array)))
     if array.ndim != 1:
         raise ValueError("argument must be of dimension 1")
-    score = 0
+
+    sortedness = 0
     for i in range(array.size):
         [left, n, right] = np.split(array, [i, i+1])
         score += np.sign(n - left).sum()
         score -= np.sign(n - right).sum()
-    return score
+    return sortedness
