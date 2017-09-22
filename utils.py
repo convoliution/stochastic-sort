1b556480362fb7f289d1a02fb04a4fcc26830c6b
Michael Liu <siphe24@gmail.com>
2017-09-22 15:47:48 -0700

    move evaluate() to separate python file

---
+++ utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def evaluate(array: np.ndarray) -> int:
+    '''Evaluate the sortedness of a np.ndarray.
+
+    More positive scores are given to arrays in ascending order;
+    More negative scores are given to arrays in descending order.
+
+    Args:
+        array: Array to be evaluated.
+
+    Returns:
+        Score earned by `array`.
+
+    Raises:
+        TypeError: If `array` is of a type other than np.ndarray.
+        ValueError: If `array` is of a dimension other than 1.
+
+    '''
+    if type(array) != np.ndarray:
+        raise TypeError("argument must be of type np.ndarray; argument was type {} instead".format(type(array)))
+    if array.ndim != 1:
+        raise ValueError("argument must be of dimension 1")
+    score = 0
+    for i in range(array.size):
+        [left, n, right] = np.split(array, [i, i+1])
+        score += np.sign(n - left).sum()
+        score -= np.sign(n - right).sum()
+    return score
